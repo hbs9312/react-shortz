37b711b8b1fcb881d62e111280d7f4974d368a57
hbs9312 <hbs30832@naver.com>
2025-04-15 21:42:24 +0900

    refactory: 불필요한 성능 최적화 훅 수정

--- core/src/useShortcut.ts
+++ core/src/useShortcut.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo } from "react";
 import { checkValidKey, checkModifierKey, keyCodeMap } from "./utils/keys";
 import { InvalidKeyError } from "./errors";
 
@@ -14,35 +14,34 @@ type ShortcutProps = {
 };
 
 export const useShortcut = (props: ShortcutProps) => {
-  const keyArray = useMemo(() => props.keys.split("+"), [props.keys]);
+  const { keys, callback } = props;
 
-  // event.altKey 등 modifier 키 확인을 위한 객체
-  const modifiers = useMemo(
-    () => ({
+  const { modifiers, normalKeys } = useMemo(() => {
+    const keyArray = keys.split("+");
+
+    // KeyboardEvent.altKey 등 modifier 키와 비교하기 위한 객체
+    const modifiers = {
       alt: keyArray.includes("alt"),
       ctrl: keyArray.includes("ctrl") || keyArray.includes("control"),
       meta: keyArray.includes("meta"),
       shift: keyArray.includes("shift"),
-    }),
-    [keyArray]
-  );
+    };
 
-  // modifier 키를 제외한 일반키 => 추후 연속키 고려하여 배열.
-  const normalKeys = useMemo(
-    () =>
-      keyArray.filter((key) => {
-        if (!checkValidKey(key)) {
-          throw new InvalidKeyError();
-        }
+    const normalKeys = keyArray.filter((key) => {
+      if (!checkValidKey(key)) {
+        throw new InvalidKeyError();
+      }
 
-        return !checkModifierKey(key);
-      }),
-    [keyArray]
-  );
+      // modifier 키가 아니라면 배열에 추가
+      return !checkModifierKey(key);
+    });
 
-  // 키 입력 이벤트 핸들러
-  const handleKeyDown = useCallback(
-    (event: KeyboardEvent) => {
+    return { modifiers, normalKeys };
+  }, [keys]);
+
+  useEffect(() => {
+    // 키 입력 이벤트 핸들러
+    const handleKeyDown = (event: KeyboardEvent) => {
       if (event.repeat) return;
 
       if (modifiers.alt && !event.altKey) return;
@@ -51,26 +50,12 @@ export const useShortcut = (props: ShortcutProps) => {
       if (modifiers.shift && !event.shiftKey) return;
 
       if (normalKeys.includes(keyCodeMap[event.code])) {
-        props.callback?.();
+        callback?.();
       }
-    },
-    [modifiers, normalKeys, props.callback]
-  );
-
-  useEffect(() => {
+    };
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [handleKeyDown]);
+  }, [modifiers, normalKeys, callback]);
 };
-
-/*
-	1. 키 등록
-		1) 문자열을 + 기준으로 분리
-		2) 분리된 문자열 중 유효한 키 인지 확인 -> KeyCode or ctlr, meta, shift, alt
-		3) 유효하지 않은 키가 포함되었다면 throw error
-		4) 수정자와 일반키를 별도로 관리
-	2. 키 입력
-
-*/
