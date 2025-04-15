3643616aafa8997d7d700fe38c7a2103f313e665
hbs9312 <hbs30832@naver.com>
2025-04-15 21:34:48 +0900

    refactory: 불필요한 타입분리 코드 수정

--- core/src/types.ts
+++ core/src/types.ts
@@ -1,29 +1,4 @@
 export type Shortcut = {
-	keys: string;
-	callback: () => void;
-};
-
-export type ShortcutProps = {
-	keys: string;
-	callback: () => void;
-	disabled?: boolean;
-	options?: {
-		preventDefault?: boolean;
-		override?: boolean;
-	};
-};
-
-export type ShortcutProviderProps = {
-	children: React.ReactNode;
-};
-
-export type ShortcutContextType = {
-	shortcuts: Shortcut[];
-	addShortcut: (shortcut: ShortcutProps) => void;
-	removeShortcut: (shortcut: Shortcut) => void;
-};
-
-export type ShortcutRefProps = {
-	keys: string;
-	ref: React.RefObject<HTMLElement | null>;
+  keys: string;
+  callback: () => void;
 };
--- core/src/useShortcut.ts
+++ core/src/useShortcut.ts
@@ -1,58 +1,68 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
-import type { ShortcutProps } from "./types";
 import { checkValidKey, checkModifierKey, keyCodeMap } from "./utils/keys";
+import { InvalidKeyError } from "./errors";
 
 // 단축키 등록
+type ShortcutProps = {
+  keys: string;
+  callback: () => void;
+  disabled?: boolean;
+  options?: {
+    preventDefault?: boolean;
+    override?: boolean;
+  };
+};
+
 export const useShortcut = (props: ShortcutProps) => {
-	const keyArray = useMemo(() => props.keys.split("+"), [props.keys]);
+  const keyArray = useMemo(() => props.keys.split("+"), [props.keys]);
 
-	// event.altKey 등 modifier 키 확인을 위한 객체
-	const modifiers = useMemo(
-		() => ({
-			alt: keyArray.includes("alt"),
-			ctrl: keyArray.includes("ctrl") || keyArray.includes("control"),
-			meta: keyArray.includes("meta"),
-			shift: keyArray.includes("shift"),
-		}),
-		[keyArray],
-	);
+  // event.altKey 등 modifier 키 확인을 위한 객체
+  const modifiers = useMemo(
+    () => ({
+      alt: keyArray.includes("alt"),
+      ctrl: keyArray.includes("ctrl") || keyArray.includes("control"),
+      meta: keyArray.includes("meta"),
+      shift: keyArray.includes("shift"),
+    }),
+    [keyArray]
+  );
 
-	// modifier 키를 제외한 일반키 => 추후 연속키 고려하여 배열.
-	const normalKeys = useMemo(
-		() =>
-			keyArray.filter((key) => {
-				if (!checkValidKey(key)) {
-					throw new Error("Modifier key is not allowed");
-				}
+  // modifier 키를 제외한 일반키 => 추후 연속키 고려하여 배열.
+  const normalKeys = useMemo(
+    () =>
+      keyArray.filter((key) => {
+        if (!checkValidKey(key)) {
+          throw new InvalidKeyError();
+        }
 
-				return !checkModifierKey(key);
-			}),
-		[keyArray],
-	);
+        return !checkModifierKey(key);
+      }),
+    [keyArray]
+  );
 
-	// 키 입력 이벤트 핸들러
-	const handleKeyDown = useCallback(
-		(event: KeyboardEvent) => {
-			if (event.repeat) return;
+  // 키 입력 이벤트 핸들러
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.repeat) return;
 
-			if (modifiers.alt && !event.altKey) return;
-			if (modifiers.ctrl && !event.ctrlKey) return;
-			if (modifiers.meta && !event.metaKey) return;
-			if (modifiers.shift && !event.shiftKey) return;
+      if (modifiers.alt && !event.altKey) return;
+      if (modifiers.ctrl && !event.ctrlKey) return;
+      if (modifiers.meta && !event.metaKey) return;
+      if (modifiers.shift && !event.shiftKey) return;
 
-			if (normalKeys.includes(keyCodeMap[event.code])) {
-				props.callback?.();
-			}
-		},
-		[modifiers, normalKeys, props.callback],
-	);
+      if (normalKeys.includes(keyCodeMap[event.code])) {
+        props.callback?.();
+      }
+    },
+    [modifiers, normalKeys, props.callback]
+  );
 
-	useEffect(() => {
-		window.addEventListener("keydown", handleKeyDown);
-		return () => {
-			window.removeEventListener("keydown", handleKeyDown);
-		};
-	}, [handleKeyDown]);
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown]);
 };
 
 /*
