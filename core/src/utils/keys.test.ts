e61b4c7165fced31828355c488ac6cb966c87c40
hbs9312 <hbs30832@naver.com>
2025-04-17 20:23:20 +0900

    test: getKey 함수 테스트 코드 작성
    - 입력언어와 관계 없이 키값을 비교하기 위해 변환하는 함수

--- core/src/utils/keys.test.ts
+++ core/src/utils/keys.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { checkModifierKey } from "./keys";
+import { checkModifierKey, getKey } from "./keys";
 
 describe("checkModifierKey()", () => {
   test("전달된 문자열이 수정자 키(ctrl, alt, shift, meta)인 경우 true를 반환한다.", () => {
@@ -13,5 +13,19 @@ describe("checkModifierKey()", () => {
     expect(checkModifierKey("a")).toBe(false);
     expect(checkModifierKey("b")).toBe(false);
     expect(checkModifierKey("c")).toBe(false);
+  });
+});
+
+describe("getKey()", () => {
+  test("전달된 키 코드를 keyCodeMap의 키로 변환한다.", () => {
+    expect(getKey("KeyA")).toBe("a");
+    expect(getKey("Digit1")).toBe("1");
+    expect(getKey("Minus")).toBe("-");
+  });
+
+  test("전달된 키 코드가 키 코드 맵에 없는 경우 null을 반환한다.", () => {
+    expect(getKey("Keyb")).toBeNull();
+    expect(getKey("Digit10")).toBeNull();
+    expect(getKey("equal")).toBeNull();
   });
 });
